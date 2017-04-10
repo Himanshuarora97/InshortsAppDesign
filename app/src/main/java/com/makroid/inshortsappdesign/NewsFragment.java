adbd94a1f3cb2d34b5d8b444f390675013b13f31
himanshu <iamhimanshuarora97@gmail.com>
2017-04-11 01:47:53 +0530

    add images

--- app/src/main/java/com/makroid/inshortsappdesign/NewsFragment.java
+++ app/src/main/java/com/makroid/inshortsappdesign/NewsFragment.java
@@ -1,4 +1,29 @@
 package com.makroid.inshortsappdesign;
 
-public class NewsFragment {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class NewsFragment extends Fragment {
+    View view;
+    private static final String NUM = "NUMBER";
+    public static NewsFragment newInstance(int number){
+        NewsFragment newsFragment = new NewsFragment();
+        Bundle args = new Bundle();
+        args.putInt(NUM,number);
+        newsFragment.setArguments(args);
+        return newsFragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.news_fragment,container,false);
+        return view;
+    }
 }
