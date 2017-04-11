2eb854a76589e494290d0af13b96b6f3fd04b6f0
himanshu <iamhimanshuarora97@gmail.com>
2017-04-11 13:41:01 +0530

    all set

--- app/src/main/java/com/makroid/inshortsappdesign/MainActivity.java
+++ app/src/main/java/com/makroid/inshortsappdesign/MainActivity.java
@@ -1,13 +1,55 @@
 package com.makroid.inshortsappdesign;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity {
+    private static final int PAGES = 20;
+    private VerticalPager pager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pager = (VerticalPager) findViewById(R.id.view_pager);
+        List<Fragment> fragmentList = new ArrayList<>();
+        for (int i = 0; i < PAGES; i++) {
+            // Number of pages in a vertical Pager
+            new NewsFragment();
+            fragmentList.add(NewsFragment.newInstance(i));
+        }
+        Adapter adapter = new Adapter(getSupportFragmentManager());
+        adapter.addFragment(fragmentList);
+        pager.setAdapter(adapter);
+        pager.setOffscreenPageLimit(0);
+
+    }
+
+
+    static class Adapter extends FragmentPagerAdapter {
+        private List<Fragment> mFragments = new ArrayList<>();
+
+        public Adapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public void addFragment(List<Fragment> fragmentList) {
+            mFragments = fragmentList;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragments.size();
+        }
     }
 }
--- app/src/main/java/com/makroid/inshortsappdesign/NewsFragment.java
+++ app/src/main/java/com/makroid/inshortsappdesign/NewsFragment.java
@@ -9,11 +9,18 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+
+import jp.wasabeef.glide.transformations.BlurTransformation;
+
 public class NewsFragment extends Fragment {
     private TextView contentText,titleText;
-    private ImageView topImage,downImage;
+    private ImageView topImage,bottomImage;
     private View view;
+    private static final int blurFactor = 200;
     private static final String NUM = "NUMBER";
+
+
     public static NewsFragment newInstance(int number){
         NewsFragment newsFragment = new NewsFragment();
         Bundle args = new Bundle();
@@ -26,6 +33,39 @@ public class NewsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.news_fragment,container,false);
+        titleText = (TextView) view.findViewById(R.id.title);
+        contentText = (TextView) view.findViewById(R.id.content);
+        topImage = (ImageView) view.findViewById(R.id.top_image);
+        bottomImage = (ImageView) view.findViewById(R.id.bottom_image);
+        setText("This is just a title of a main app, says President","Lorem ipsum dolor sit amet, consectetur elit adipisicing , sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim minim ad  veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.");
+        switch (getArguments().getInt(NUM) % 5) {
+            case 0:
+                Glide.with(getActivity()).load(R.drawable.image1).into(topImage);
+                Glide.with(getActivity()).load(R.drawable.image1).bitmapTransform(new BlurTransformation(getContext(),blurFactor)).into(bottomImage);
+                break;
+            case 1:
+                Glide.with(getActivity()).load(R.drawable.image2).into(topImage);
+                Glide.with(getActivity()).load(R.drawable.image2).bitmapTransform(new BlurTransformation(getContext(),blurFactor)).into(bottomImage);
+                break;
+            case 2:
+                Glide.with(getActivity()).load(R.drawable.image3).into(topImage);
+                Glide.with(getActivity()).load(R.drawable.image3).bitmapTransform(new BlurTransformation(getContext(),blurFactor)).into(bottomImage);
+                break;
+            case 3:
+                Glide.with(getActivity()).load(R.drawable.image4).into(topImage);
+                Glide.with(getActivity()).load(R.drawable.image4).bitmapTransform(new BlurTransformation(getContext(),blurFactor)).into(bottomImage);
+                break;
+
+            default:
+                Glide.with(getActivity()).load(R.drawable.image5).into(topImage);
+                Glide.with(getActivity()).load(R.drawable.image5).bitmapTransform(new BlurTransformation(getContext(),blurFactor)).into(bottomImage);
+
+        }
         return view;
+    }
+
+    private void setText(String title, String content) {
+        titleText.setText(title);
+        contentText.setText(content);
     }
 }
